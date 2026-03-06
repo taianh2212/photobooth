4ae56dfe5c4dd50b7f4cbd48df7f3e291148d6d6
taianh2212 <taianh22122004@gmail.com>
2026-03-06 17:27:15 +0700

    Update config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // You can optionally uncomment the lines below if your app is hosted under a subdirectory like: https://taianh2212.github.io/photobook2/
+  basePath: '/photobook2',
+  assetPrefix: '/photobook2/',
   typescript: {
     ignoreBuildErrors: true,
   },
